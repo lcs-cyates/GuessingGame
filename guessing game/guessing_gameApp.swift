39089cad33985faccaf892ebb524eb5b604b961b
Clea Yates <cyates@lcs.on.ca>
2020-11-17 14:53:34 -0500

    Initial Commit

---
+++ guessing game/guessing_gameApp.swift
@@ -0,0 +1,17 @@
+//
+//  guessing_gameApp.swift
+//  guessing game
+//
+//  Created by Yates, Clea on 2020-11-17.
+//
+
+import SwiftUI
+
+@main
+struct guessing_gameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
