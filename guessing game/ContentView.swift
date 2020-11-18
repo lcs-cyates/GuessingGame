f6fd63a12f4ad8b02e0f8afb14439088e3674c0b
Clea Yates <cyates@lcs.on.ca>
2020-11-18 13:52:30 -0500

    got a basic reset game function implemented

--- guessing game/ContentView.swift
+++ guessing game/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
    // The guess made by the user
     @State private var theUserGuess = ""
+    @State private var target = Int.random(in: 1...100)
+    @State private var feedback = ""
     var body: some View{
         NavigationView{
             
@@ -17,6 +19,8 @@ struct ContentView: View {
         VStack{
             
             Text("Guessing Game")
+                .font(.title)
+                .padding(.vertical)
             
             Text("I'm thinking of a number between 1 and 100. Guess what it is!")
             
@@ -25,10 +29,25 @@ struct ContentView: View {
             Button ("Submit Guess"){
                 //check the guess
             }
+            .padding(.all)
             
             Text("You guessed\(theUserGuess)")
+            
+            Text("\(feedback)")
+            guard let givenInteger = Int(theUserGuess)
         }
+        .padding(.all)
         .navigationTitle("Guessing Game")
+    }
+    func checkGuess(){
+    }
+    func resetGame(){
+        target = Int.random(in: 1...100)
+        theUserGuess = ""
+        feedback = ""
+        Button("Reset Game"){
+            resetGame()
+        }
     }
 }
 
