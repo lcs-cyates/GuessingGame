6b2b5abb6ac2d691a1a07d1900e3e44f8475b90b
Clea Yates <cyates@lcs.on.ca>
2020-11-20 12:12:42 -0500

    fixed syntax errors and added what code i was missing

--- guessing game/ContentView.swift
+++ guessing game/ContentView.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-   // The guess made by the user
-    @State private var theUserGuess = ""
+    
+    //The number that the user should guess
     @State private var target = Int.random(in: 1...100)
+    
+    //Feedback to the user
     @State private var feedback = ""
+    
+    //Current guess made by the user
+    @State private var theUserGuess = ""
+    
+    //prior guess made by the user
+    @State private var priorGuess = ""
+    
+    //keep track of if the game is over
+    @State private var gameOver = false
+    
     var body: some View{
-        NavigationView{
+        
+        NavigationView {
             
-        }
+        
         VStack{
             
             Text("Guessing Game")
@@ -25,28 +38,68 @@ struct ContentView: View {
             Text("I'm thinking of a number between 1 and 100. Guess what it is!")
             
             TextField("Enter your guess here", text: $theUserGuess)
+                .disabled(gameOver)
             
-            Button ("Submit Guess"){
+            Button ("Check Guess"){
                 //check the guess
+                checkGuess()
             }
             .padding(.all)
+            .disabled(gameOver)
             
-            Text("You guessed\(theUserGuess)")
+            if priorGuess.conut > 0 {
+                Text("Your last guess was \(priorGuess).")
             
-            Text("\(feedback)")
-            guard let givenInteger = Int(theUserGuess)
-        }
-        .padding(.all)
-        .navigationTitle("Guessing Game")
+                Text("\(feedback)")
+                
+            }
+            
+            if gameOver {
+                Button("New game"){
+                    resetGame()
+                }
+                Spacer()
+            }
+            .navigationTitle("Guessing Game")
     }
+     
+  //check what the user guessed against the target
     func checkGuess(){
-    }
-    func resetGame(){
-        target = Int.random(in: 1...100)
-        theUserGuess = ""
-        feedback = ""
-        Button("Reset Game"){
-            resetGame()
+        // see if the given intager in the expected range
+        guard let givenInteger = Int(theUserGuess) else {
+            feedback = "Please provide an integer between 1 and 100."
+            return
+            
+        }
+        guard givenInteger > 0, givenInteger < 101 else {
+            feedback = "Please provide an integer between 1 and 100."
+            return
+        }
+            
+            // is the guess correct?
+            let range = givenInteger - target
+            switch range {
+            case 1...:
+                feedback = "Guess lower next time."
+            case 0:
+                feedback = "Thats correct! Well done."
+                gameOver = true
+            default:
+                feedback = "Guess higher next time."
+            }
+            
+            //Reset the user's guess
+            priorGuess = theUserGuess
+            theUserGuess = ""
+        }
+        
+            //Reset the game
+    
+        func resetGame(){
+            target = Int.random(in: 1...100)
+            theUserGuess = ""
+            priorGuess = ""
+            gameOver = false
         }
     }
 }
@@ -56,3 +109,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+    }
+
